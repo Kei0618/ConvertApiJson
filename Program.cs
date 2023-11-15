ecc04586fa18d9a1be7faff45449a751e0226666
Kei0618 <adsf8564@gmail.com>
2023-11-15 10:11:27 +0800

    read json ok

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
 namespace ConvertApiJson
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"C:\Users\JerryLin\Desktop\test.json";
+            using (FileStream fileRead = new FileStream(args[0], FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(fileRead))
+                {
+                    string? result = reader.ReadToEnd();
+                    System.Console.WriteLine(result);
+                }
+
+            }
+
         }
     }
 }
