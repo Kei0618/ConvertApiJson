924a0cfbd4ef4122c690c82e4a47c11416cedd43
LIYAOLI-TM56NHA\Administrator <adsf8564@gmail.com>
2023-11-21 23:15:46 +0800

    rename

--- Services/CheckVariant.cs
+++ Services/CheckVariant.cs
@@ -16,19 +16,19 @@ namespace ConvertApiJson
             {
                 if (!File.Exists(_readFilePath))
                 {
-                    System.Console.WriteLine("參數1錯誤:讀取路徑檔案不存在");
+                    System.Console.WriteLine("參數一錯誤:讀取路徑檔案不存在");
 
                     return false;
                 }
                 else if (strings[1].Contains('/'))
                 {
-                    System.Console.WriteLine("參數2錯誤:寫入路徑錯誤，url為參數3");
+                    System.Console.WriteLine("參數二錯誤:寫入路徑錯誤，url為參數3");
 
                     return false;
                 }
                 else if (strings[2].Contains('\\'))
                 {
-                    System.Console.WriteLine("參數3錯誤:url錯誤");
+                    System.Console.WriteLine("參數三錯誤:url錯誤");
 
                     return false;
                 }
@@ -38,55 +38,57 @@ namespace ConvertApiJson
                     return true;
                 }
             }
+
             else if (strings.Length > 3)
             {
-                System.Console.WriteLine("參數錯誤:輸入超過三個參數");
+                System.Console.WriteLine("參數錯誤:輸入超過三個參數\n需求參數為\n參數一:讀取路徑\n參數二:寫入路徑\n參數三:url");
                 return false;
             }
-            else
+
+            else if (strings.Length == 2)
             {
-                if (strings.Length == 2)
-                {
-                    if (File.Exists(_readFilePath) && strings[1].Contains('/'))
-                    {
-                        System.Console.WriteLine("錯誤:\n參數二錯誤:寫入路徑無效\n參數三錯誤:缺少url參數");
-                        return false;
-                    }
-                    else if (File.Exists(_readFilePath) && strings[1].Contains('\\'))
-                    {
-                        System.Console.WriteLine("錯誤:\n參數三錯誤:缺少url參數");
-                        return false;
-                    }
-                    else if (!File.Exists(_readFilePath) && strings[1].Contains('/'))
-                    {
-                        System.Console.WriteLine("錯誤:\n參數一錯誤:讀取路徑無效\n參數二錯誤:寫入路徑無效\n參數三錯誤:缺少url參數");
-                        return false;
-                    }
-                    else if (!File.Exists(_readFilePath) && strings[1].Contains('\\'))
-                    {
-                        System.Console.WriteLine("錯誤:參數一錯誤:讀取路徑無效\n參數三錯誤:缺少url參數");
-                        return false;
-                    }
 
-                }
-                else if (strings.Length == 1)
+                if (File.Exists(_readFilePath) && strings[1].Contains('/'))
                 {
-                    if (File.Exists(_readFilePath))
-                    {
-                        System.Console.WriteLine("錯誤:\n參數二錯誤:缺少寫入路徑\n參數三錯誤:缺少url參數");
-                        return false;
-                    }
-                    else if (!File.Exists(_readFilePath) && strings[0].Contains('\\'))
-                    {
-                        System.Console.WriteLine("錯誤:\n參數一錯誤:讀取路徑無效\n參數二錯誤:缺少寫入路徑\n參數三錯誤:缺少url參數");
-                        return false;
-                    }
+                    System.Console.WriteLine("錯誤:\n參數二錯誤:寫入路徑無效\n參數三錯誤:缺少url參數");
+                    return false;
                 }
-
-                System.Console.WriteLine("參數錯誤:缺少以下參數\n參數一:讀取路徑\n參數二:寫入路徑\n參數三:url");
-                return false;
+                else if (File.Exists(_readFilePath) && strings[1].Contains('\\'))
+                {
+                    System.Console.WriteLine("錯誤:\n參數三錯誤:缺少url參數");
+                    return false;
+                }
+                else if (!File.Exists(_readFilePath) && strings[1].Contains('/'))
+                {
+                    System.Console.WriteLine("錯誤:\n參數一錯誤:讀取路徑無效\n參數二錯誤:寫入路徑無效\n參數三錯誤:缺少url參數");
+                    return false;
+                }
+                else if (!File.Exists(_readFilePath) && strings[1].Contains('\\'))
+                {
+                    System.Console.WriteLine("錯誤:參數一錯誤:讀取路徑無效\n參數三錯誤:缺少url參數");
+                    return false;
+                }
 
             }
+
+            else if (strings.Length == 1)
+            {
+                if (File.Exists(_readFilePath))
+                {
+                    System.Console.WriteLine("錯誤:\n參數二錯誤:缺少寫入路徑\n參數三錯誤:缺少url參數");
+                    return false;
+                }
+                else
+                {
+                    System.Console.WriteLine("錯誤:\n參數一錯誤:讀取路徑無效\n參數二錯誤:缺少寫入路徑\n參數三錯誤:缺少url參數");
+                    return false;
+                }
+            }
+
+            System.Console.WriteLine("參數錯誤:缺少以下參數\n參數一:讀取路徑\n參數二:寫入路徑\n參數三:url");
+            return false;
+
+
         }
 
     }
